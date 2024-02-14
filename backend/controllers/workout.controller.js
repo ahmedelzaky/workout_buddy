d5b332af16bb329a1394091d23d74bad5fd0d5c2
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2024-02-14 19:16:39 +0200

    Refactor workout controller and add workout model

--- backend/controllers/workout.controller.js
+++ backend/controllers/workout.controller.js
@@ -1,8 +1,64 @@
-const getAllWorkout = async (req, res) => {};
-const addWorkout = async (req, res) => {};
-const getWorkout = async (req, res) => {};
-const deleteWorkout = async (req, res) => {};
-const updateWorkout = async (req, res) => {};
+const workoutModel = require("../models/workout.model");
+const WorkoutModel = require("../models/workout.model");
+
+const getAllWorkout = async (req, res) => {
+  const workouts = await workoutModel.find().exec();
+  res.json(workouts);
+};
+
+const addWorkout = async (req, res) => {
+  try {
+    const workout = new WorkoutModel(req.body);
+    await workout.save();
+    res.status(201).json({ success: true, workout });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const workout = await WorkoutModel.findById(id).exec();
+    if (workout) {
+      res.json({ success: true, workout });
+    } else {
+      res
+        .status(404)
+        .json({ success: false, message: "There is no Workout with this id" });
+    }
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let workout = await WorkoutModel.findByIdAndUpdate(id, req.body).exec();
+    if (workout) {
+      workout = await WorkoutModel.findById(id).exec();
+      res.json({ success: true, workout });
+    } else {
+      res
+        .status(404)
+        .json({ success: false, message: "There is no Workout with this id" });
+    }
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await WorkoutModel.findByIdAndDelete(id).exec();
+    res.json({ success: true });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
 
 module.exports = {
   getAllWorkout,
