e20a863cea40216a73be5ef83d534dd6a86c5585
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2024-02-14 15:14:07 +0200

    Add dotenv dependency and configure port dynamically

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const swagger = require("./swagger");
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api-docs", swagger.serve, swagger.setup);
 
 app.listen(port, () => {
-  console.log("app started on http://localhost:3000");
+  console.log(`app started on http://localhost:${port}`);
 });
