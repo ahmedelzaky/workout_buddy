43347b2bc9f9fb536c9444d159cdb5b62b9ed865
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2024-02-15 19:04:23 +0200

    Add bcrypt and validator dependencies, implement signup functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,9 +1,18 @@
-const UserModel = require("../models/user.model");
+const userModel = require("../models/user.model");
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 };
-const signup = async (req, res) => {};
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.signup(email, password);
+
+    res.status(201).json({ success: true, user });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
 module.exports = {
   login,
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -7,12 +9,32 @@ const userSchema = new mongoose.Schema(
       unique: true,
       required: true,
     },
-    passwrod: {
+    password: {
       type: String,
       required: true,
     },
   },
   { timestamps: true }
 );
+
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (!email || !password) throw Error("All field must be filled");
+
+  if (!validator.isEmail(email)) throw Error("Email is not valid");
+
+  if (!validator.isStrongPassword(password))
+    throw Error("Password not strong enough");
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+  password = await bcrypt.hash(password, process.env.SALT);
+
+  const user = await this.create({ email, password });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
